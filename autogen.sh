365931e95420daea997dbb021ffba8a6fc580015
adamsampson <adamsampson>
2006-11-10 21:39:06 +0000

    Make autogen.sh just use autoreconf, and rebuild all the auto* bits.

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-#gettextize --no-changelog \
-true \
-  && aclocal -I m4 \
-  && libtoolize -f -c \
-  && autoheader \
-  && automake -a -c \
-  && autoconf
-
-if test x$1 != x--no-conf; then
-  if test -e conf; then
-    sh conf;
-  fi
-fi
+autoreconf -f -i
